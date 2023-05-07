00c6f6ce3e3e8674704888c226239a97b110bb76
ImashNipun <it21034404@my.sliit.lk>
2023-05-07 18:12:44 +0530

    Design a interace to display a single recipe

--- frontend/src/components/Recipe/AllRecipes.jsx
+++ frontend/src/components/Recipe/AllRecipes.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React,{useState} from "react";
 import { Container, Row, Col, Button, Form } from "react-bootstrap";
 import "./AllRecipes.style.css";
 import RecipeItem from "./RecipeItem";
+import DisplayARecipe from "../Admin/Recipe/DisplayARecipe";
 
 export default function AllRecipes() {
 
+  const [modalShow, setModalShow] = useState(false);
 
-
+  const handleModel = (e) =>{
+    setModalShow(true)
+  }
 
   return (
     <Container fluid>
@@ -22,11 +26,14 @@ export default function AllRecipes() {
             </Col>
           </Row>
           <Row>
-            <Col md={2} className="bg-primary filter-sec">
+            <Col md={2} className="filter-sec" style={{backgroundColor:"rgb(216, 234, 255)"}}>
               Filter sec
             </Col>
+            <DisplayARecipe 
+            show={modalShow}
+            onHide={() => setModalShow(false)} />
             <Col className="recipe-list overflow-scroll">
-              <RecipeItem />
+              <RecipeItem  handleM={handleModel} rid={1}/>
               <RecipeItem />
               <RecipeItem />
               <RecipeItem />
